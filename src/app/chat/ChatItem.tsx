0c4f9b304d339fab5044d9309cb131442c0e0390
mzhafranh <88304603+mzhafranh@users.noreply.github.com>
2024-12-10 21:27:20 +0700

    fix chat style

--- src/app/chat/ChatItem.tsx
+++ src/app/chat/ChatItem.tsx
@@ -2,19 +2,19 @@ export default function ChatItem() {
     return (
         <>
             <div className="flex items-start">
-                <div className="bg-white font-sans text-black px-4 py-2 rounded-full max-w-xs border border-solid mb-2">
+                <div className="bg-white font-sans text-black px-4 py-2 rounded-2xl max-w-sm border border-solid mb-2 break-words">
                     Halo
                 </div>
             </div>
 
             <div className="flex items-start">
-                <div className="bg-white font-sans text-black px-4 py-2 rounded-full max-w-xs border border-solid mb-20">
+                <div className="bg-white font-sans text-black px-4 py-2 rounded-2xl max-w-sm border border-solid mb-20 break-words">
                     Lagi dimana?
                 </div>
             </div>
 
             <div className="flex items-start justify-end">
-                <div className="bg-blue-500 font-sans text-white px-4 py-2 rounded-full max-w-xs mb-2">
+                <div className="bg-blue-500 font-sans text-white px-4 py-2 rounded-2xl max-w-sm mb-2 break-words">
                     Lagi di rumah bang
                 </div>
             </div>
