08aa9a1c985acc2523b4a47efa7e3eddcbf02b03
mzhafranh <88304603+mzhafranh@users.noreply.github.com>
2024-12-12 22:58:54 +0700

    chat api get

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,2 +1,15 @@
+import prisma from "../../../../lib/prisma";
+import { NextResponse } from "next/server";
+
+
 export async function GET() {
+    try {
+        const users = await prisma.message.findMany();
+        return NextResponse.json(users, { status: 200 });
+    } catch (error) {
+        return NextResponse.json(
+            { error: "Failed to fetch users" },
+            { status: 500 }
+        );
+    }
 }
