0dbc96149fe19657be61ea5b97dd1f22642072e2
mzhafranh <muhammad.zhafran.haris@gmail.com>
2024-12-21 18:44:42 +0700

    log out button done

--- src/app/chat/ContactBox.tsx
+++ src/app/chat/ContactBox.tsx
@@ -1,12 +1,18 @@
 import ContactList from "./ContactList";
 
 export default function ContactBox() {
+    const handleLogout = () => {
+        localStorage.removeItem('authToken');
+        localStorage.removeItem('refreshToken');
+        window.location.href = "/login";
+    };
+    
     return (
         <div className="container h- w-2/12 rounded-lg shadow-centered shadow-slate-500 mr-5 flex flex-col">
             <h1 className="font-bold text-gray-500 text-center text-2xl p-4">Contacts</h1>
             <hr className="mb-4" />
             <ContactList/>
-            <button className="text-red-500 border font-bold text-sm border-red-500 rounded-lg px-5 py-2 mx-12 my-4 hover:bg-red-600 hover:text-white focus:bg-red-800">LOG OUT</button>
+            <button onClick={handleLogout} className="text-red-500 border font-bold text-sm border-red-500 rounded-lg px-5 py-2 mx-12 my-4 hover:bg-red-600 hover:text-white focus:bg-red-800">LOG OUT</button>
         </div>
     )
 }
