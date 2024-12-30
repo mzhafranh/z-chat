8fa4f4a10dd271d7a1ba627f934f27c65cee487e
mzhafranh <muhammad.zhafran.haris@gmail.com>
2024-12-30 15:05:07 +0700

    added notification count

--- src/app/chat/ContactItem.tsx
+++ src/app/chat/ContactItem.tsx
@@ -1,6 +1,9 @@
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "../store/store";
 import { setCurrentContact } from "../store/slices/chatSlice";
+import { getSocket } from "../components/SocketProvider";
+import { useEffect, useRef, useState } from "react";
+
 
 interface ContactItemProps {
     id: string;
@@ -11,10 +14,32 @@ interface ContactItemProps {
 
 const ContactItem: React.FC<ContactItemProps> = ({ id, username, currentContact, currentUser}) => {
     const dispatch = useDispatch<AppDispatch>();
+    const socket = getSocket()
+    const currentContactRef = useRef<string | null>(null);
+    const [unreadCount, setUnreadCount] = useState<number>(0);
+
+    useEffect(() => {
+        currentContactRef.current = currentContact;
+    }, [currentContact]);
+
+    useEffect(() => {
+        socket.on(`${currentUser}`, (message) => {
+            if (message.senderId === username && message.recipientId === currentUser && currentContactRef.current != username){
+                console.log(`Contactlist received message ${currentUser}`)
+                setUnreadCount((prevCount) => prevCount + 1);
+            }
+        })
+        return () => {
+            socket.off(`${username}`);
+        };
+    }, [socket])
+
 
     const handleChangeContact = () => {
         dispatch(setCurrentContact(username))
+        setUnreadCount(0);
     }
+
     if (username === currentUser) {
         return
     }
@@ -24,7 +49,10 @@ const ContactItem: React.FC<ContactItemProps> = ({ id, username, currentContact,
                 {username === currentContact ? (
                     <button className="text-white text-md w-full text-left bg-blue-500 px-4 py-2">{username}</button>
                 ) : (
-                    <button className="text-gray-500 w-full text-md px-4 py-2 text-left hover:bg-blue-500 hover:text-white" onClick={handleChangeContact}>{username}</button>
+                    <button className="text-gray-500 w-full text-md px-4 py-2 text-left hover:bg-blue-500 hover:text-white" onClick={handleChangeContact}>
+                        {username}
+                        {unreadCount > 0 && <span className="ml-2 text-sm text-red-500">({unreadCount})</span>}
+                        </button>
                 )}
             </>
         )
