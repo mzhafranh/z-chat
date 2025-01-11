bd2cbe27158155c8e852e2805142569300cc9ab4
mzhafranh <muhammad.zhafran.haris@gmail.com>
2025-01-11 21:14:32 +0700

    added logout remove refreshtoken

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -65,3 +65,35 @@ export async function POST(req: Request) {
         
     }
 }
+
+export async function PUT(req: Request) {
+    try {
+        const body = await req.json();
+        const { username } = body;
+
+        // Validate input
+        if (!username) {
+            return NextResponse.json(
+                { error: "User ID is required." },
+                { status: 400 }
+            );
+        }
+
+        // Update the user to remove the refresh token
+        const updatedUser = await prisma.user.update({
+            where: { username: username },
+            data: { refreshToken: null },
+        });
+
+        return NextResponse.json(
+            { message: "Logout successful.", user: updatedUser },
+            { status: 200 }
+        );
+    } catch (error) {
+        console.error("Error during logout:", error);
+        return NextResponse.json(
+            { error: "Failed to logout user.", details: error.message },
+            { status: 500 }
+        );
+    }
+}
