cb7a4607aad82c14ab35c28e5f905afde6a26ba1
mzhafranh <muhammad.zhafran.haris@gmail.com>
2025-01-15 10:39:09 +0700

    removed unused codes

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -10,7 +10,6 @@ import { useRouter } from 'next/navigation'
 
 export default function Page() {
   const router = useRouter()
-  const token = typeof window !== 'undefined' ? localStorage.getItem('authToken') : null;
   const refreshToken = typeof window !== 'undefined' ? localStorage.getItem('refreshToken') : null;
   const currentUser = typeof window !== 'undefined' ? localStorage.getItem('zchatuser') : null;
   const dispatch = useDispatch<AppDispatch>();
@@ -18,23 +17,12 @@ export default function Page() {
 
 
   useEffect(() => {
-    if (!token || !currentUser) {
+    if (!refreshToken || !currentUser) {
       router.push('/login')
     } else {
       dispatch(autoLogin({refreshToken}))
-      // dispatch(login(currentUser))
-      // dispatch(verifyToken({ token }))
-      //   .then((action) => {
-      //     if (!action.payload) {
-      //       router.push('/login')
-      //     }
-      //   })
-      //   .catch((err) => {
-      //     console.error("Error verifying token:", err);
-      //     router.push('/login')
-      //   });
     }
-  }, [token, refreshToken, dispatch]);
+  }, [refreshToken, dispatch]);
 
   return (
     <div className="flex justify-center h-screen items-center bg-gray-900 text-gray-100">
